1c59038ed5973e9f5e35e222a179915da3bdc328
vinifillmann <vinifillmann08@gmail.com>
2022-09-11 20:56:40 -0300

    Iniciando projeto

---
+++ resources/views/series/index.blade.php
@@ -0,0 +1,12 @@
+<x-layout title="Séries">
+    <ul>
+        @foreach ($series as $serie)
+            <li>{{ $serie }}</li>
+        @endforeach
+    </ul>
+
+    <script>
+        const series = {{ Js::from($series) }}
+    </script>
+
+</x-layout>
