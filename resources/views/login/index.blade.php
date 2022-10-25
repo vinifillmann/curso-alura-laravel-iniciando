355a9a1c2bfe8282e5b3845cec3a195a56049bdd
vinifillmann <vinifillmann08@gmail.com>
2022-10-25 09:17:34 -0300

    Avançando no curso

---
+++ resources/views/login/index.blade.php
@@ -0,0 +1,14 @@
+<x-layout title="Login">
+    <form method="POST">
+        @csrf
+        <div class="form-group">
+            <label for="email" class="form-label">E-mail</label>
+            <input type="email" name="email" id="email" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="password" class="form-label">Senha</label>
+            <input type="password" name="password" id="password" class="form-control">
+        </div>
+        <input class="btn btn-primary mt-3" type="submit" value="Entrar">
+    </form>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
-use App\Http\Controllers\EpisodesController;
-use App\Http\Controllers\SeasonsController;
-use App\Http\Controllers\SeriesController;
 use Illuminate\Http\Client\Request;
+use App\Http\Middleware\Autenticador;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\SeriesController;
+use App\Http\Controllers\SeasonsController;
+use App\Http\Controllers\EpisodesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,7 +21,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return redirect("/series");
-});
+})->middleware(Autenticador::class);
 
 Route::resource("/series", SeriesController::class)
     ->except(["show"]);
@@ -28,6 +30,9 @@ Route::get("/series/{series}/seasons", [SeasonsController::class, "index"])->nam
 
 Route::get("/seasons/{season}/episodes", [EpisodesController::class, "index"])->name("episodes.index");
 Route::post("/seasons/{season}/episodes", [EpisodesController::class, "update"])->name("episodes.update");
+
+Route::get("/login", [LoginController::class, "index"])->name("login");
+Route::post("/login", [LoginController::class, "store"])->name("login.store");
 
 // Route::controller(SeriesController::class)->group(function () {
 //     Route::get("/series", "index")->name("series.index");
